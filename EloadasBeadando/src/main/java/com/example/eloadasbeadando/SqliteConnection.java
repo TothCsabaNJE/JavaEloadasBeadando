5a1a2cfba29c60abbc6961af274333d40ab60d9a
Tóth Csaba <lagolas420@gmail.com>
2024-11-30 23:51:03 +0100

    Update SqliteConnection.java

--- EloadasBeadando/src/main/java/com/example/eloadasbeadando/SqliteConnection.java
+++ EloadasBeadando/src/main/java/com/example/eloadasbeadando/SqliteConnection.java
@@ -7,7 +7,7 @@ public class SqliteConnection {
     public static Connection Connector() {
         try {
             Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\adatbazis\\Cukraszda.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\adatbazis\\adatok.db");
             return conn;
         } catch (Exception e) {
             System.out.println(e);
